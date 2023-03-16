052eda2ff46530ba64ea84aa1453d056a7225cd9
naixsu <plnacua02@gmail.com>
2023-03-16 23:12:55 +0800

    added water interactions to player
    also fixed some bugs esp gravity when in water

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -6,16 +6,32 @@ public class Water : MonoBehaviour
 {
 
     [SerializeField] private PlayerMovement player;
-    // Start is called before the first frame update
-    void Start()
+
+    private bool inWater;
+    public float waterGravity; 
+    public float waterSpeed;
+    public float waterJump;
+
+    private void Start()
     {
-        
+        inWater = false;
+        //waterGravity = player.data.gravityScale / 2;
+        waterGravity = 1;
+        waterSpeed = player.data.defaultMoveSpeed / 2;
+        waterJump = player.data.defaultJumpPower / 4;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (inWater) // decrease stamina while in water
+        {
+            player.data.stamina -= player.data.waterStaminaDrain * Time.deltaTime;
+            
+        }
+        else
+        {
+            
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -23,6 +39,11 @@ public class Water : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Debug.Log(true);
+            inWater = true;
+            player.data.speed = waterSpeed;
+            player.data.jumpPower = waterJump;
+            player.rb.gravityScale = waterGravity;
+            
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -30,6 +51,10 @@ public class Water : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Debug.Log(false);
+            inWater = false;
+            player.SetGravityScale(player.data.gravityScale);
+            player.data.speed = player.data.defaultMoveSpeed;
+            player.data.jumpPower = player.data.defaultJumpPower;
         }
     }
 }
