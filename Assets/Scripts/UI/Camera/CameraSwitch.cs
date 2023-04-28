e12940d406f975c3da8d9d3f5cea93bb33958799
naixsu <plnacua02@gmail.com>
2023-04-28 17:50:40 +0800

    Sirao Cameras

--- Assets/Scripts/UI/Camera/CameraSwitch.cs
+++ Assets/Scripts/UI/Camera/CameraSwitch.cs
@@ -7,29 +7,21 @@ using Unity.VisualScripting;
 public class CameraSwitch : MonoBehaviour
 {
     public CinemachineVirtualCamera cam;
-    public static CinemachineVirtualCamera activeCamera = null;
-
-    void SwitchCamera(CinemachineVirtualCamera camera)
-    {
-        camera.Priority = 10;
-        activeCamera = camera;
-
-        foreach (CinemachineVirtualCamera vcam in CameraManager.Instance.cameras)
-        {
-            if (vcam != camera && vcam.Priority != 0)
-            {
-                vcam.Priority = 0;
-                vcam.gameObject.SetActive(false);
-            }
-        }
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && activeCamera != cam)
+        if (collision.gameObject.CompareTag("Player"))
         {
             cam.gameObject.SetActive(true);
-            SwitchCamera(cam);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            cam.gameObject.SetActive(false);
         }
     }
 }
