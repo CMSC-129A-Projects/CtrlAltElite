1c0b81326acc6e2defd9b5ae6c609c2878a035f0
naixsu <plnacua02@gmail.com>
2023-05-05 19:30:48 +0800

    Respawn

---
+++ Assets/Scripts/Player Stuff/Death.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Death : MonoBehaviour
+{
+    public string currentBoundary;
+    public BoxCollider2D currentCollider;
+    public GameObject currentRespawn;
+    [SerializeField] private float _respawnTimer;
+    [SerializeField] private float _animationTimer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Boundary"))
+        {
+            currentBoundary = collision.gameObject.name;
+            currentCollider = collision.gameObject.GetComponent<BoxCollider2D>();
+            currentRespawn = collision.transform.GetChild(0).gameObject;
+        }
+
+        if (collision.gameObject.CompareTag("Spikes"))
+        {
+            StartCoroutine(StartRespawn());
+        }
+    }
+
+    private IEnumerator StartRespawn()
+    {
+        var _deathScript = transform.GetComponent<TestMovement2>();
+        // _deathScript.SetGravityScale(0);
+        TestMovement2.isDead = true;
+        TestMovement2.canMove = false;
+        transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        // transform.GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(_respawnTimer);
+        // transform.GetComponent<SpriteRenderer>().enabled = true;
+        transform.position = currentRespawn.transform.position;
+        transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        yield return new WaitForSeconds(_animationTimer);
+        TestMovement2.isDead = false;
+        TestMovement2.canMove = true;
+        
+        // _deathScript.SetGravityScale(_deathScript.data.gravityScale);
+    }
+}
