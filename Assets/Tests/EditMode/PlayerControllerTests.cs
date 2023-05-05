fa93dda571978dfe792507e62664688d5aac524d
ChennyChicken <wygulane@up.edu.ph>
2023-05-05 17:11:23 +0900

    revert

--- Assets/Tests/EditMode/PlayerControllerTests.cs
+++ Assets/Tests/EditMode/PlayerControllerTests.cs
@@ -16,25 +16,6 @@ public class PlayerControllerTests
         Assert.AreEqual(1,1);
     }
 
-    [Test]
-    public void TestHurt()
-    {
-        GameObject player = new GameObject("Player");
-        PlayerController playerController = player.AddComponent<PlayerController>();
-
-        // Arrange
-        player.health = 100;
-
-        // Act
-        player.hurt(2);
-
-        // Assert
-        Debug.Log(player.health);
-        Assert.AreEqual(player.health, 98);
-
-
-        
-    }
 
 
 }
