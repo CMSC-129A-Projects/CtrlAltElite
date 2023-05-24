05accbd78437fac4ae4f53dc50fa0eef63cae220
pkingjohn13 <kbpareja@up.edu.ph>
2023-05-24 15:46:22 +0800

    city 4 bg
    added bgs for F city 4

--- Assets/Scripts/Map Stuff/Water.cs
+++ Assets/Scripts/Map Stuff/Water.cs
@@ -41,7 +41,7 @@ public class Water : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
