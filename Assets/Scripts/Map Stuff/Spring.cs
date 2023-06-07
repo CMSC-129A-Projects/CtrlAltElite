a67a41dd5f31409c7fb8845d96c38ce9d5001957
naixsu <plnacua02@gmail.com>
2023-06-07 20:03:36 +0800

    spring animation function

--- Assets/Scripts/Map Stuff/Spring.cs
+++ Assets/Scripts/Map Stuff/Spring.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class Spring : MonoBehaviour
 {
     [SerializeField] private float bounce;
+    private Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            SetSpringUp();
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
         }
+    }
+
+    public void SetSpringUp()
+    {
+        animator.SetTrigger("SpringUp");
+    }
+
+    public void SetSpringIdle()
+    {
+        animator.SetTrigger("SpringIdle");
     }
 }
