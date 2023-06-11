9f265805934c12d18b38e3221aad8fe3dc8fe7d9
naixsu <plnacua02@gmail.com>
2023-06-11 14:01:33 +0800

    Fade in out lore animation

--- Assets/Scripts/UI/Map/Flags.cs
+++ Assets/Scripts/UI/Map/Flags.cs
@@ -7,13 +7,65 @@ using UnityEngine.EventSystems;
 public class Flags : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private GameObject lore;
+    private CanvasGroup canvasGroup;
+    public bool fadeOutAnimation = false;
+    public bool fadeInAnimation = false;
+    private float _timer = 0f;
+    [SerializeField] private float fadeInTime;
+    [SerializeField] private float fadeOutTime;
+
+    private void Start()
+    {
+        canvasGroup = lore.GetComponent<CanvasGroup>();
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
+        fadeInAnimation = true;
+        fadeOutAnimation = false;
+        _timer = 0f;
         lore.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        lore.SetActive(false);
+        //lore.SetActive(false);
+        fadeInAnimation = false;
+        fadeOutAnimation = true;
+    }
+
+    private void Update()
+    {
+        if (fadeInAnimation)
+        {
+            _timer += Time.deltaTime;
+            // Calculate the normalized progress of the animation
+            float progress = _timer / fadeInTime;
+            // Increase the image's color alpha based on the progress
+            canvasGroup.alpha = progress;
+
+            if (_timer >= fadeInTime)
+            {
+                _timer = 0f;
+                fadeInAnimation = false;
+            }
+        }
+
+        if (fadeOutAnimation)
+        {
+            _timer += Time.deltaTime;
+
+            // Calculate the normalized progress of the animation
+            float progress = _timer / fadeOutTime;
+
+            // Reduce the image's color alpha based on the progress
+            canvasGroup.alpha = 1f - progress;
+
+            if (_timer >= fadeOutTime)
+            {
+                _timer = 0f;
+                fadeOutAnimation = false;
+                lore.SetActive(false);
+            }
+        }
     }
 }
