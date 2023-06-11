90e5fc9d70d5531a95d30cb67108e08c2fdcfbb2
naixsu <plnacua02@gmail.com>
2023-06-11 23:07:33 +0800

    Finished transitions

--- Assets/Scripts/UI/NewSaves/Loader.cs
+++ Assets/Scripts/UI/NewSaves/Loader.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class Loader : MonoBehaviour
 {
     [SerializeField] private GameObject transition;
+    [SerializeField] private SugboMovement player;
     private void Awake()
     {
         Debug.Log("Loader Awake");
+        
         transition.SetActive(true);
 
+    }
+
+    private void Start()
+    {
+        if (player != null) 
+        {
+            DontAllowMovePlayer();
+            StartCoroutine(AllowMovePlayer());
+        }
+        
+    }
+
+    private void DontAllowMovePlayer()
+    {
+        Debug.Log("LOADER DONT ALLOW MOVE");
+        player.inTransition = true;
+    }
+
+    private IEnumerator AllowMovePlayer()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("LOADER ALLOW MOVE PLAYER");
+        player.inTransition = false;
     }
 }
