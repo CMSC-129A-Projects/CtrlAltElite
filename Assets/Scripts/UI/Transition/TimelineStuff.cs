c9ac69fdb47c7ebb922e53577c68613bfd04e9eb
naixsu <plnacua02@gmail.com>
2023-06-14 22:18:28 +0800

    Can skip intro cutscene

--- Assets/Scripts/UI/Transition/TimelineStuff.cs
+++ Assets/Scripts/UI/Transition/TimelineStuff.cs
@@ -6,7 +6,39 @@ using UnityEngine.SceneManagement;
 public class TimelineStuff : MonoBehaviour
 {
     [SerializeField] private Animator basePlayerAnimator;
+    [SerializeField] private ConfirmationPopupMenu confirmationPopupMenu;
+    private bool pressedEsc = false;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pressedEsc)
+            {
+                // If the confirmation menu is active, close it
+                confirmationPopupMenu.gameObject.SetActive(false);
+                pressedEsc = false;
+            }
+            else
+            {
+                confirmationPopupMenu.ActivateMenu(
+                    "Skip cutscene?",
+                    // function to execute if we select 'yes'
+                    () =>
+                    {
+                        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+                    },
+                    // function to execute if we select 'no'
+                    () =>
+                    {
+                    // do nothing
+                    }
+                );
+                pressedEsc = true;
+            }
+            
+        }
+    }
     public void PlayBaseIdleAnim()
     {
         Debug.Log("PlayingIdle");
@@ -29,4 +61,26 @@ public class TimelineStuff : MonoBehaviour
         // SceneManager.LoadSceneAsync("City 5");
         SceneManager.LoadSceneAsync("F City 4");
     }
+
+    /*public void OnYesClicked()
+    {
+        confirmationPopupMenu.ActivateMenu(
+                "Skip cutscene?",
+                // function to execute if we select 'yes'
+                () =>
+                {
+                    SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+                },
+                // function to execute if we select 'cancel'
+                () =>
+                {
+                    
+                }
+            );
+    }
+
+    public void OnNoClicked()
+    {
+        this.gameObject.SetActive(false);
+    }*/
 }
